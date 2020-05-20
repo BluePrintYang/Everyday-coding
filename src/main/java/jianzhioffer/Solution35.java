7d8ee6380c3fd65e2542e1274fa93c88383d0680
BluePrintYang <1671158yllizb@sina.com>
2020-05-20 21:22:28 +0800

    剑指offer 第一个只出现一次的字符

--- src/main/java/jianzhioffer/Solution35.java
+++ src/main/java/jianzhioffer/Solution35.java
@@ -10,13 +10,13 @@ import java.util.Map;
  */
 public class Solution35 {
     public static int FirstNotRepeatingChar(String str) {
-        Map<Character,Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < str.length(); i++) {
-            if (map.containsKey(str.charAt(i)))map.put(str.charAt(i),map.get(str.charAt(i))+1);
-            else map.put(str.charAt(i),1);
+            if (map.containsKey(str.charAt(i))) map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+            else map.put(str.charAt(i), 1);
         }
         for (int i = 0; i < str.length(); i++) {
-            if (map.get(str.charAt(i))==1)return i;
+            if (map.get(str.charAt(i)) == 1) return i;
         }
         return -1;
     }
