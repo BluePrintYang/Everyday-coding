4ba42a56f0a9022a753b4d8db3b84e40c8728f62
BluePrintYang <1671158yllizb@sina.com>
2020-05-23 01:32:03 +0800

    JUC 加法计数器CyclicBarrier

--- src/main/java/kuangshenjava/juc/CyclicBarrierDemo.java
+++ src/main/java/kuangshenjava/juc/CyclicBarrierDemo.java
@@ -7,10 +7,8 @@ import java.util.concurrent.CyclicBarrier;
  * 加法计数器
  */
 public class CyclicBarrierDemo {
-    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
-        CyclicBarrier c = new CyclicBarrier(7, () -> {
-            System.out.println("计数器到7");
-        });
+    public static void main(String[] args) {
+        CyclicBarrier c = new CyclicBarrier(7, () -> System.out.println("计数器到7"));
         for (int i = 0; i < 7; i++) {
             //lambda表达式里面只能是final变量
             final int temp = i + 1;
@@ -19,9 +17,7 @@ public class CyclicBarrierDemo {
 
                 try {
                     c.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (BrokenBarrierException e) {
+                } catch (InterruptedException | BrokenBarrierException e) {
                     e.printStackTrace();
                 }
 
