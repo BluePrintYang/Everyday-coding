fcc9fff6e6defe344d5877d53f958b8285522bdc
BluePrintYang <1671158yllizb@sina.com>
2020-06-28 20:40:54 +0800

    直接内存OOM

---
+++ src/main/java/vm/DirectMemoryOOM.java
@@ -0,0 +1,23 @@
+package vm;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @Author yangllong
+ * @Date 2020/6/8 16:01
+ */
+public class DirectMemoryOOM {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+        while (true){
+            unsafe.allocateMemory(_1MB);
+        }
+    }
+}
